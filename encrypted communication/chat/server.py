a7d0eb36d872eca3ab4ef6a9763776c4ab4a095f
robinsjangid <95579617+robinsjangid@users.noreply.github.com>
2022-04-16 11:20:44 -0400

    File created

---
+++ encrypted communication/chat/server.py
@@ -0,0 +1,5 @@
+# SERVER CODE
+# Update 1.1 AES Encryption Now Included :p
+# AES Block Cipher 16 bytes block size as supported by pyaes
+# You Can Change the Port Server Listens by passing argument in command line directly
+# Server Code To be Started before Client, or Connection will be refused
