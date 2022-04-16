72780766963411c7e278c2b69e65f4a75f9e2391
robinsjangid <95579617+robinsjangid@users.noreply.github.com>
2022-04-16 11:17:43 -0400

    creating a daemon thread and starting a thread

--- encrypted communication/chat/client.py
+++ encrypted communication/chat/client.py
@@ -117,3 +117,26 @@ class myThread(threading.Thread):
                         print('Unrecognised Data or Broken PIPE ')
             except ConnectionResetError:
                 print('Broken PIPE!')
+# creating a daemon thread and starting a thread
+Listening_Thread = myThread(1)
+Listening_Thread.daemon = True
+Listening_Thread.start()
+
+
+while 1:
+    try:
+        sending_data = str(input(""))
+    except KeyboardInterrupt:
+        s.close()
+        exit(-1)
+    if(sending_data=="quit()"):
+        Listening_Thread.stop()
+        s.close()
+        exit()
+    timestamp = str(datetime.now())[11:19]
+    mess_hash = hashlib.sha256(str(sending_data).encode('utf-8')).hexdigest()
+    send_data = {
+        "timestamp" : timestamp,
+        "message"   : sending_data,
+        "hash"      : mess_hash
+    }
