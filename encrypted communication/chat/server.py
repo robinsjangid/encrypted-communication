e346871d5c966939995712eda6be5563b80120d4
robinsjangid <95579617+robinsjangid@users.noreply.github.com>
2022-04-16 11:21:33 -0400

    added import statement

--- encrypted communication/chat/server.py
+++ encrypted communication/chat/server.py
@@ -3,3 +3,34 @@
 # AES Block Cipher 16 bytes block size as supported by pyaes
 # You Can Change the Port Server Listens by passing argument in command line directly
 # Server Code To be Started before Client, or Connection will be refused
+
+import os
+try:
+    import pyaes
+except ImportError:
+    print("Install pyaes library!")
+    print("windows : python -m pip insatll pyaes")
+    print("linux   : pip install pyaes ")
+    exit()
+# import statements
+import sys
+import socket
+import threading
+import hashlib
+import json
+from datetime import datetime
+
+
+HOST = '0.0.0.0'
+if(len(sys.argv)==1):
+    PORT = 5555
+elif(len(sys.argv)==2):
+    PORT=int(sys.argv[1])
+
+
+print("[+] Server Running ")
+print("[+] Allowing All Incoming Connections ")
+print("[+] PORT "+str(PORT))
+print("[+] Waiting For Connection...")
+
+
