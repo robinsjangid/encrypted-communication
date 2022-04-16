4fc30611a9a2f8ff515e254236cca45ab090abb6
robinsjangid <95579617+robinsjangid@users.noreply.github.com>
2022-04-16 11:06:37 -0400

    Files created

---
+++ encrypted communication/chat/client.py
@@ -0,0 +1,13 @@
+# CLIENT CODE
+# Update 1.1 AES Encryption Now Included :p
+# AES Block Cipher 16 bytes block size as supported by pyaes
+# Server Code Must Be Running Before Starting Client or Connection will be refused
+
+import os
+try:
+    import pyaes
+except ImportError:
+    print("Install pyaes library!")
+    print("windows : python -m pip insatll pyaes")
+    print("linux   : pip install pyaes ")
+    exit()
